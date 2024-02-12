fdfac69eda58a2424dd542ffce30abe386b553aa
CarlosHernandez14 <carloskvpchc@gmail.com>
2024-02-12 00:32:45 -0600

    Animations on buttons added

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,73 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [noCount, setNoCount] = useState(0);
 
+  const sound = new Audio('/src/assets/videoplayback.m4a');
+
+  const handleNoClick = () => {
+    setNoCount(noCount + 1);
+    // Modify the size of the yes button respect to noCount value
+    const yesBtn = document.getElementById('yes-btn');
+    //yesBtn.style.fontSize = `${noCount * 2}rem`;
+
+    // Asign the pulse class to the yes button
+    yesBtn.classList.add('pulse');
+
+    // Remove the pulse class aftert animation ends
+    yesBtn.addEventListener('animationend', () => {
+      yesBtn.classList.remove('pulse');
+    });
+    
+    const noBtn = document.getElementById('no-btn');
+
+    noBtn.classList.add('shake');
+
+    noBtn.addEventListener('animationend', () => {
+      noBtn.classList.remove('shake');
+    });
+
+    switch (noCount) {
+      case 0:
+        alert('Estas segura?');
+        break;
+      case 1:
+        alert('De verdad no quieres ser mi valentin?');
+        break;
+      case 2:
+        alert('Andale no seas mala, di que si');
+        break;
+      case 3:
+        alert('Yo si quiero ser tu valentin');
+        break;
+      default:
+        alert('No puedes decir que no, no te dejare ir');
+        break;
+    }
+  }
+
+  const handleYesClick = () => {
+
+    if (!sound.paused) {
+      sound.pause();
+      sound.currentTime = 0;
+    }
+
+    sound.play();
+  }
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='main-container'>
+        <img src="/src/assets/mocha3.gif" alt="No se pudo cargar la imagen :(" />
+        <h1>¿Quieres ser mi valentin?</h1>
+        <div className='container-buttons'>
+          <button id='yes-btn' onClick={handleYesClick}>Si</button>
+          <button id='no-btn' onClick={handleNoClick}>No</button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
 
-export default App
+export default App;
